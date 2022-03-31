1bbea6a215a7634eb222894ec2f70dd72217cb72
Mat(2.718)i <mateimurarasu@yahoo.com>
2022-03-31 16:34:44 +0300

    Transfer pe Visual Studio

---
+++ proiect/proiect/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proiect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clienti c1 = new clienti("Andrei", "Marco", "Suceava");
+            c1.Get_data();
+        }
+    }
+}

6dc19c3f2730eb189eb226b37e6d996f7d9d7132
Mat(2.718)i <mateimurarasu@yahoo.com>
2022-04-25 20:30:53 +0300

    Revert "Changelog v0.3 ( verificati fisierul .txt pentru mai multe informatii)"
    This reverts commit 4021c97b87c1b311c0792774929244c1e80c50ad.

---
+++ proiect/proiect/clase/clienti.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proiect
+{
+    public class clienti
+    {
+        private string nume;
+        private string prenume;
+        private string adresa;
+        public clienti()
+        {
+            nume = string.Empty;
+            prenume = string.Empty;
+            adresa = string.Empty;
+        }
+        public clienti(string nume,string prenume,string adresa)
+        {
+            this.nume = nume;
+            this.prenume = prenume; 
+            this.adresa = adresa;
+        }
+        public string Nume { get { return nume; } set { nume = value; } }
+        public string Prenume { get { return prenume; } set { prenume = value; } }
+        public string Adresa { get { return adresa; } set { adresa = value; } }
+        public void Get_data() { Console.WriteLine(nume + " | " + prenume + " | " + adresa); }
+    }
+}
